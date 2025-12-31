51426d329b8051c93e177a18d99a68682d93ae99
RAFEEK-S <rafeeksarthar73@gmail.com>
2025-12-31 16:49:48 +0530

    first commit

---
+++ arrays.js
@@ -0,0 +1,7 @@
+const engineer = ["Rafeek",34,"B.E chemical Engg",2012];
+
+
+console.log("engineer name:",engineer[0])
+console.log("engineer age:",engineer[1])
+console.log("engineer qualification:",engineer[2])
+console.log("engineer year of passing:",engineer[3])
