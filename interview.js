0cd0750db241146ec8bff769e7f1f09fcb7c0836
RAFEEK-S <rafeeksarthar73@gmail.com>
2026-01-04 02:18:22 +0530

    commit

--- interview.js
+++ interview.js
@@ -1,1 +1,16 @@
+//objects:
+const a = {
+    age:23
+}
 
+b = a;
+a.age = 100;
+console.log(a)  // here it will go to the reference and change the value;
+
+
+console.log(typeof []);
+
+const sorted = [100,150,50,200];
+
+const result = sorted.sort((a,b)=>a-b);
+console.log(result);
